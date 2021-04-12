bc54ab0aa3179825cf8c6f5b6446ad0378a71e24
Sriram Srirangam <sriram.n.srirangam@gmail.com>
2021-04-11 23:58:50 -0500

    Fill in About page

--- doomsday_trainer/src/AboutPage.jsx
+++ doomsday_trainer/src/AboutPage.jsx
@@ -1,38 +1,34 @@
-import { useState } from "react";
-import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-
 const AboutPage = () => {
   return (
     <div className="General-app-text-content App-page-with-bar">
       <div className="Paragraph-block">
-        <h2 style={{ color: "black" }}>
+        <h1 style={{ color: "black" }}>
           <b>
             <u>About</u>
           </b>
-        </h2>
+        </h1>
         <p>
           {" "}
-          The{" "}
-          <a
-            className="Info-link"
-            href="https://en.wikipedia.org/wiki/Doomsday_rule"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Doomsday algorithm
-          </a>{" "}
-          is a method to mentally compute the day of the week for a given date.
-          This web application is designed to both teach and provide practice to
-          facilitate learning the skill. This web application is designed to
-          both teach and provide practice to facilitate learning the skill.{" "}
+          The <b>Doomsday algorithm</b> is a method to mentally compute the day
+          of the week for a given date. This web application is designed to both
+          teach and provide practice to facilitate learning this skill.{" "}
         </p>
         <p>
           {" "}
-          This web application is designed to both teach and provide practice to
-          facilitate learning the skill.{" "}
+          The 'Learn' mode of the application contains sections of content that
+          teach the algorithm step by step. Some sections also have practice at
+          the end for the content covered in that section. New users who want to
+          learn the algorithm for the first time will want to start with this
+          mode.{" "}
         </p>
-        <p> The 'Learn' page </p>
+        <p>
+          {" "}
+          The 'Train' mode of the application offers practice problems for
+          training the ability to perform the entire algorithm, along with
+          various metrics such as scorekeeping, average response time, etc.
+          Returning users who have already completed the previous mode will want
+          to go here.{" "}
+        </p>
       </div>
     </div>
   );
