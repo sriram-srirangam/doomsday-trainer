1546848b5853a9678e0b36b7440751552507a766
Sriram Srirangam <sriram.n.srirangam@gmail.com>
2021-04-11 21:47:49 -0500

    About page formatting

--- doomsday_trainer/src/AboutPage.jsx
+++ doomsday_trainer/src/AboutPage.jsx
@@ -5,24 +5,35 @@ import ButtonGroup from "@material-ui/core/ButtonGroup";
 const AboutPage = () => {
   return (
     <div className="General-app-text-content App-page-with-bar">
-      <p>
-        {" "}
-        The{" "}
-        <a
-          className="Info-link"
-          href="https://en.wikipedia.org/wiki/Doomsday_rule"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Doomsday algorithm
-        </a>{" "}
-        is a method to mentally compute the day of the week for a given date.{" "}
-      </p>
-      <p>
-        {" "}
-        This web application is designed to both teach and provide practice to
-        facilitate learning the skill. The 'Learn' page{" "}
-      </p>
+      <div className="Paragraph-block">
+        <h2 style={{ color: "black" }}>
+          <b>
+            <u>About</u>
+          </b>
+        </h2>
+        <p>
+          {" "}
+          The{" "}
+          <a
+            className="Info-link"
+            href="https://en.wikipedia.org/wiki/Doomsday_rule"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Doomsday algorithm
+          </a>{" "}
+          is a method to mentally compute the day of the week for a given date.
+          This web application is designed to both teach and provide practice to
+          facilitate learning the skill. This web application is designed to
+          both teach and provide practice to facilitate learning the skill.{" "}
+        </p>
+        <p>
+          {" "}
+          This web application is designed to both teach and provide practice to
+          facilitate learning the skill.{" "}
+        </p>
+        <p> The 'Learn' page </p>
+      </div>
     </div>
   );
 };
